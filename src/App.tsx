85e543559bfafcf4b6989b155f4e297f07ab6b37
gabriel-assana <gabrielhhz2@gmail.com>
2025-02-22 15:15:45 -0300

    feat: criando layouts e configurando React Helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { Button } from './components/ui/button'
 import './global.css'
+
+import { QueryClientProvider } from '@tanstack/react-query'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
+
+import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
+import { router } from './routes'
 
 export function App() {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <Button>Enviar</Button>
-    </div>
+    <HelmetProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
+
+        <Toaster richColors />
+
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </HelmetProvider>
   )
 }
-
-
