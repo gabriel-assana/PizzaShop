b5f542abf1c603e0ad0cd5670801de75318e8aec
gabriel-assana <gabrielhhz2@gmail.com>
2025-02-22 14:36:02 -0300

    feat: instalando shadcn/ui e configurando ESLint e Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
+import { Button } from './components/ui/button'
+import './global.css'
 
 export function App() {
   return (
-    <>
-      <h1>Ola Mundo</h1>
-    </>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Enviar</Button>
+    </div>
   )
 }
 
