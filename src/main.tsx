7a8f6ad0d48d42e9a9c8a42ebae795ba718388fe
gabriel-assana <gabrielhhz2@gmail.com>
2025-03-23 19:45:25 -0300

    feat: instalando e motificando para o MSW

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './global.css'
-import { App } from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { enableMSW } from './api/mocks'
+import { App } from './app'
+
+enableMSW().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
